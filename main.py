a21fa36bf2d31d6621d3b505eb179efcb4dfcc2c
MZalapa <zalapamiguel9@gmail.com>
2023-12-07 13:03:49 -0800

    made my variables

--- main.py
+++ main.py
@@ -6,3 +6,37 @@ Add a sword
 Mobs that attack my player
 My player dies when the mobs run into him
 '''
+import pygame, sys, random, math, time
+from pygame.locals import*
+pygame.init()
+
+SCREEN = pygame.display.set_mode((1280, 720), pygame.RESIZABLE)
+FPS = 30
+CLOCK = pygame.time.Clock()
+pygame.display.set_caption("Agar.io")
+
+cell_count = 2000
+bot_count = 20
+map_size = 2000
+spawn_size = 25
+bots_min_size = 25
+bots_max_size = 250
+respawn_cells = True
+respawn_bots = False
+player_color = (255, 0, 0)
+background_color = (0, 0, 0)
+text_color = (255, 255, 255)
+
+FONT = pygame.font.Font("freesansbold.ttf", 32)
+FONT = pygame.font.Font("freesansbold.ttf", 72)
+WIDTH = 1280
+HEIGHT = 720
+cells = []
+bots = []
+game_over = False
+
+counter = 0
+frame_rate =30
+start_time = 0
+frame_rate_delay = 0.5
+
